fcd63468d2562314dd69860f1688afa01c83d105
saúl z mtz <97975590+szmtz@users.noreply.github.com>
2023-10-20 14:32:44 +0000

    Add new html and js

--- script.js
+++ script.js
@@ -1,32 +1,38 @@
-var data = [
-    { fruit: "Apples", count: 10 },
-    { fruit: "Oranges", count: 15 },
-  ];
+var data = [10, 20, 30, 40, 50];
 
+var svg = d3.select("svg");
+var barWidth = 40;
+var barSpacing = 10;
 
-  // Select the chart container
-var svg = d3.select("#chart-container")
-.append("svg")
-.attr("width", 400)
-.attr("height", 200);
+svg
+  .selectAll("rect")
+  .data(data)
+  .enter()
+  .append("rect")
+  .attr("x", function (d, i) {
+    return i * (barWidth + barSpacing);
+  })
+  .attr("y", function (d) {
+    return 200 - d;
+  })
+  .attr("width", barWidth)
+  .attr("height", function (d) {
+    return d;
+  })
+  .attr("class", "bar");
 
-// Create a rectangle for each data point
-svg.selectAll("rect")
-.data(data)
-.enter()
-.append("rect")
-.attr("x", function(d, i) { return i * 80; })
-.attr("y", function(d) { return 200 - d.count * 10; })
-.attr("width", 50)
-.attr("height", function(d) { return d.count * 10; })
-.attr("fill", "orange");
-
-// Add labels
-svg.selectAll("text")
-.data(data)
-.enter()
-.append("text")
-.text(function(d) { return d.fruit; })
-.attr("x", function(d, i) { return i * 80 + 25; })
-.attr("y", 190)
-.attr("text-anchor", "middle");
+  svg
+  .selectAll("text")
+  .data(data)
+  .enter()
+  .append("text")
+  .attr("x", function (d, i) {
+    return i * (barWidth + barSpacing) + barWidth / 2;
+  })
+  .attr("y", function (d) {
+    return 200 - d - 5;
+  })
+  .attr("text-anchor", "middle")
+  .text(function (d) {
+    return d;
+  });
